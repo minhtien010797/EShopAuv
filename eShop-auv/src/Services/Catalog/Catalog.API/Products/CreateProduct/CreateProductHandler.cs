c5c5d343621b96e1ae91db266e09c21b678f2be8
thinh.dang.duc@stepmediasoftware.com <thinh.dang.duc@dmchosting.ca>
2025-06-01 20:35:22 +0700

    push code

---
+++ eShop-auv/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Create entity
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // Save to DB
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // Return result
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
